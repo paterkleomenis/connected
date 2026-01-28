42f1d22a0099fae372ff246897eb2f218a62bc11
paterkleomenis <alexisbasakidze@gmail.com>
2026-01-28 21:54:31 +0200

    chore(release): bump version to 1.0.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ configure<ApplicationExtension> {
         applicationId = "com.connected.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
