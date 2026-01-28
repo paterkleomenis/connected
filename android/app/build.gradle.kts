65f066dfb00df9e872734d0fa62e38ae198978e5
paterkleomenis <alexisbasakidze@gmail.com>
2026-01-28 15:25:16 +0200

    feat: 1.0.1
    Co-authored-by: IOANNIS SPYROPOULOS <135458101+redjackgr@users.noreply.github.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
