9d9175bad1942f7e98facc24ca2016ed7908ef40
paterkleomenis <alexisbasakidze@gmail.com>
2026-02-02 00:27:04 +0200

    2.2.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ configure<ApplicationExtension> {
         applicationId = "com.connected.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 33
-        versionName = "2.2.7"
+        versionCode = 34
+        versionName = "2.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
