a48b88025043736a2e11aa13608c09d505e3612f
paterkleomenis <alexisbasakidze@gmail.com>
2026-05-12 12:58:11 +0200

    3.0.5

--- PERMISSION_JUSTIFICATION.md
+++ PERMISSION_JUSTIFICATION.md
@@ -2,7 +2,7 @@
 
 **App Name:** Connected  
 **Package:** com.connected.app.sync  
-**Version:** 3.0.4  
+**Version:** 3.0.5  
 **Date:** May 12, 2026
 
 ---
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,8 +51,8 @@ configure<ApplicationExtension> {
         applicationId = "com.connected.app.sync"
         minSdk = 26
         targetSdk = 37
-        versionCode = 59
-        versionName = "3.0.4"
+        versionCode = 60
+        versionName = "3.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
